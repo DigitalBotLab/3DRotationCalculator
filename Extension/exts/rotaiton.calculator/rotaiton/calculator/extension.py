ec715f286eab65f027fd80aa902f3594a156a4ad
yizhouzhao <yizhou_zhao@berkeley.edu>
2023-07-11 17:26:45 -0700

    update

--- Extension/exts/rotaiton.calculator/rotaiton/calculator/extension.py
+++ Extension/exts/rotaiton.calculator/rotaiton/calculator/extension.py
@@ -1,12 +1,10 @@
 import omni.ext
 import omni.ui as ui
 
-
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
-def some_public_function(x: int):
-    print("[rotaiton.calculator] some_public_function was called with x: ", x)
-    return x ** x
-
+from .ui.custom_multifield_widget import CustomMultifieldWidget
+from pxr import Gf
+import numpy as np
+from .numpy_utils import euler_angles_to_quat
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
@@ -19,25 +17,69 @@ class RotaitonCalculatorExtension(omni.ext.IExt):
 
         self._count = 0
 
-        self._window = ui.Window("My Window", width=300, height=300)
+        self._window = ui.Window("3D Rotation Calculator", width=300, height=100, visible=True)
         with self._window.frame:
             with ui.VStack():
-                label = ui.Label("")
+                with ui.CollapsableFrame("Quaternion mul", collapsed=False, height = 0):
+                    with ui.VStack():
+                        with ui.HStack(height = 20):
+                            self.q1_widget = CustomMultifieldWidget(
+                                label="Quaternion 1:",
+                                sublabels=[ "w", "x", "y", "z"],
+                                default_vals=[1, 0, 0, 0],
+                            )
+                        with ui.HStack(height = 20):
+                            self.q2_widget = CustomMultifieldWidget(
+                                label="Quaternion 2:",
+                                sublabels=[ "w", "x", "y", "z"],
+                                default_vals=[1, 0, 0, 0],
+                            )
+                        ui.Line(height = 5) 
+                        with ui.HStack(height = 20):
+                            self.q_widget = CustomMultifieldWidget(
+                                label="Result:",
+                                sublabels=[ "w", "x", "y", "z"],
+                                default_vals=[1, 0, 0, 0],
+                                read_only= True
+                            )                
+                        self.q_str_widget = ui.StringField(width = 200, height = 20)
+                        ui.Line(height = 5)
+                        with ui.HStack():
+                            ui.Button("Quaternion mul", height = 20, clicked_fn=self.quaternioin_mul)
 
-
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+                with ui.CollapsableFrame("Euler to Quaternion", collapsed=False, height = 0):
+                    with ui.VStack():
+                        with ui.HStack(height = 20):
+                            self.euler_widget = CustomMultifieldWidget(
+                                label="Euler angles (degree):",
+                                sublabels=[ "roll", "pitch", "yaw"],
+                                default_vals=[0, 0, 0],
+                            )
+                        ui.Line(height = 5)
+                        with ui.HStack(height = 20):
+                            self.quat_widget = CustomMultifieldWidget(
+                                label="Quaternion:",
+                                sublabels=[ "w", "x", "y", "z"],
+                                default_vals=[1, 0, 0, 0],
+                                read_only= True
+                            )
+                        ui.Button("Euler to Quat", height = 20, clicked_fn=self.euler2quat)
 
     def on_shutdown(self):
         print("[rotaiton.calculator] rotaiton calculator shutdown")
+
+    def quaternioin_mul(self):
+        print("quaternioin_mul")
+        q1 = [self.q1_widget.multifields[i].model.as_float for i in range(4)] # wxyz
+        q1 = Gf.Quatf(q1[0], q1[1], q1[2], q1[3])
+        q2 = [self.q2_widget.multifields[i].model.as_float for i in range(4)] # wxyz
+        q2 = Gf.Quatf(q2[0], q2[1], q2[2], q2[3])
+        q = q1 * q2
+        self.q_widget.update([q.GetReal(), *q.GetImaginary()]) 
+        self.q_str_widget.model.set_value(str(q))
+
+    def euler2quat(self):
+        print("euler2quat")
+        euler = [self.euler_widget.multifields[i].model.as_float for i in range(3)] # roll pitch yaw
+        q = euler_angles_to_quat(euler, degrees=True).tolist()
+        self.quat_widget.update(q)
